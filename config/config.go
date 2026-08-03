78047908d5f7a794b1326f54d348c92f1e807781
farhanlumoshive <farhan.arfianto@lumoshive.com>
2026-08-03 14:36:03 +0700

    feat: add refresh news endpoint and support for refresh token

--- config/config.go
+++ config/config.go
@@ -3,28 +3,30 @@ package config
 import "os"
 
 type Config struct {
-	Port       string
-	DBHost     string
-	DBPort     string
-	DBUser     string
-	DBPass     string
-	DBName     string
-	RedisAddr  string
-	NewsAPIURL string
-	NewsAPIKey string
+	Port         string
+	DBHost       string
+	DBPort       string
+	DBUser       string
+	DBPass       string
+	DBName       string
+	RedisAddr    string
+	NewsAPIURL   string
+	NewsAPIKey   string
+	RefreshToken string
 }
 
 func Load() *Config {
 	return &Config{
-		Port:       getEnv("PORT", "8080"),
-		DBHost:     getEnv("DB_HOST", "localhost"),
-		DBPort:     getEnv("DB_PORT", "5432"),
-		DBUser:     getEnv("DB_USER", "postgres"),
-		DBPass:     getEnv("DB_PASS", "postgres"),
-		DBName:     getEnv("DB_NAME", "newsdb"),
-		RedisAddr:  getEnv("REDIS_ADDR", "localhost:6379"),
-		NewsAPIURL: getEnv("NEWS_API_URL", "https://newsapi.org/v2/everything?q=European%20football&language=en&sortBy=publishedAt"),
-		NewsAPIKey: getEnv("NEWS_API_KEY", ""),
+		Port:         getEnv("PORT", "8080"),
+		DBHost:       getEnv("DB_HOST", "localhost"),
+		DBPort:       getEnv("DB_PORT", "5432"),
+		DBUser:       getEnv("DB_USER", "postgres"),
+		DBPass:       getEnv("DB_PASS", "postgres"),
+		DBName:       getEnv("DB_NAME", "newsdb"),
+		RedisAddr:    getEnv("REDIS_ADDR", "localhost:6379"),
+		NewsAPIURL:   getEnv("NEWS_API_URL", "https://newsapi.org/v2/everything?q=European%20football&language=en&sortBy=publishedAt"),
+		NewsAPIKey:   getEnv("NEWS_API_KEY", ""),
+		RefreshToken: getEnv("REFRESH_TOKEN", ""),
 	}
 }
 
